9e94457ec6c69289e2169a839103810d435af886
Osmanys <ophernandez95@gmail.com>
2021-10-08 18:30:59 -0500

    finished with styling

--- src/Characters.js
+++ src/Characters.js
@@ -3,11 +3,23 @@ import styled from "styled-components";
 
 
 
-const Character = styled.div`
-    background:red;
+const Char = styled.div`
+    font-size: 1.5rem;
+    display: grid;
+    grid-template-columns: 3fr 1fr;
 
 `
-
+const CharInfo = styled.div`
+    width:100%;
+    display: none;
+`
+const ExpandButton = styled.div`
+    width:100%;
+    grid-column-start:2 span;
+    grid-row-start:2;
+    font-size: 1rem;
+    font-family: Arial Black;
+`
 
 const Characters = (props) => {
 const {name, birthYear} = props
@@ -22,8 +34,18 @@ const {name, birthYear} = props
     return (
             
         
-            <div className='test'>
-                <Character>{name}{birthYear}</Character>
+            <div className='char-wrapper'>
+                <Char>
+                    <div className='name'>{name}</div>
+                    <div className='year'>{birthYear}</div>
+                    <ExpandButton>EXPAND</ExpandButton>
+                </Char>
+                <CharInfo>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </CharInfo>
             </div> 
             
 
